614be68e5750331b02baa0f1f75c1b87c79d4843
影月 零 <zero@genshin.org>
2014-07-25 16:37:02 +0900

    update rasters from vectors, fixed naming

--- util/update_rasters.rb
+++ util/update_rasters.rb
@@ -14,7 +14,9 @@ require 'emojidex_converter'
 require 'emojidex-vectors'
 
 # UTF
-converter = Emojidex::Converter.new(destination: '../emoji/utf')
+utf_dest = '../emoji/utf'
+FileUtils.rm_rf(utf_dest)
+converter = Emojidex::Converter.new(destination: utf_dest)
 utf = Emojidex::UTF.new
 
 puts "Rasterizing UTF emoji"
@@ -22,7 +24,9 @@ puts "入[#{utf.source_path}] 出[#{converter.destination}]"
 converter.rasterize_collection(utf)
 
 # Extended
-converter = Emojidex::Converter.new(destination: '../emoji/extended')
+extended_dest = '../emoji/extended'
+FileUtils.rm_rf(extended_dest)
+converter = Emojidex::Converter.new(destination: extended_dest)
 extended = Emojidex::Extended.new
 
 puts "Rasterizing Extended emoji"
