80d332bda605503b67381e749020e8be195d9685
影月 零 <zero@genshin.org>
2016-05-09 21:35:56 +0900

    update images, remove paths, properly fill checksums

--- util/update_rasters.rb
+++ util/update_rasters.rb
@@ -9,22 +9,29 @@ require 'emojidex/data/extended'
 require 'emojidex_converter'
 require 'emojidex-vectors'
 
-# UTF
-utf_dest = '../emoji/utf'
-FileUtils.rm_rf(utf_dest)
-converter = Emojidex::Converter.new(destination: utf_dest, noisy: true)
-utf = Emojidex::Data::UTF.new
+def convert_utf
+  # UTF
+  utf_dest = '../emoji/utf'
+  FileUtils.rm_rf(utf_dest)
+  converter = Emojidex::Converter.new(destination: utf_dest, noisy: true)
+  utf = Emojidex::Data::UTF.new(r18: true)
 
-puts "Rasterizing UTF emoji"
-puts "入[#{utf.source_path}] 出[#{converter.destination}]"
-converter.rasterize_collection(utf)
+  puts "Rasterizing UTF emoji"
+  puts "入[#{utf.source_path}] 出[#{converter.destination}]"
+  converter.rasterize_collection(utf)
+end
 
-# Extended
-extended_dest = '../emoji/extended'
-FileUtils.rm_rf(extended_dest)
-converter = Emojidex::Converter.new(destination: extended_dest, noisy: true)
-extended = Emojidex::Data::Extended.new
+def convert_extended
+  # Extended
+  extended_dest = '../emoji/extended'
+  FileUtils.rm_rf(extended_dest)
+  converter = Emojidex::Converter.new(destination: extended_dest, noisy: true)
+  extended = Emojidex::Data::Extended.new(r18: true)
 
-puts "Rasterizing Extended emoji"
-puts "入[#{extended.source_path}] 出[#{converter.destination}]"
-converter.rasterize_collection(extended)
+  puts "Rasterizing Extended emoji"
+  puts "入[#{extended.source_path}] 出[#{converter.destination}]"
+  converter.rasterize_collection(extended)
+end
+
+convert_extended
+convert_utf
